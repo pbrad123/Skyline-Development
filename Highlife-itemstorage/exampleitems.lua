850d73f8602cef34492e08fa1ea48c9145a6d6fb
Skyline Development <119904266+pbrad123@users.noreply.github.com>
2025-10-03 23:59:45 -0500

    Delete Highlife-itemstorage directory

--- Highlife-itemstorage/exampleitems.lua
+++
@@ -1,7 +0,0 @@
--- ## Example Shared items.lua for qbcore
- -- Be sure to use the following as a template in your shared items.lua file in your qbcore section.
- -- You will need to create each item in your shared items.lua file before it can be used in the config.
-
- -- ['cigarettebox'] 				 = {['name'] = 'cigarettebox', 			  	  	['label'] = 'Cigarette Box', 			['weight'] = 13, 		['type'] = 'item', 		['image'] = '69-brand-pack.png', 		['unique'] = true, 		['useable'] = true, 	['shouldClose'] = true,    ['combinable'] = nil,   ['description'] = 'A cigarette box for holding cigarettes'},
- -- ['tacklebox'] 				 = {['name'] = 'tacklebox', 			  	  	['label'] = 'tacklebox', 			['weight'] = 13, 		['type'] = 'item', 		['image'] = 'tacklebox.png', 		['unique'] = true, 		['useable'] = true, 	['shouldClose'] = true,    ['combinable'] = nil,   ['description'] = 'A Tacklebox for fishing Storage'},
- -- ['wallet'] 				 = {['name'] = 'wallet', 			  	  	['label'] = 'wallet', 			['weight'] = 13, 		['type'] = 'item', 		['image'] = 'wallet.png', 		['unique'] = true, 		['useable'] = true, 	['shouldClose'] = true,    ['combinable'] = nil,   ['description'] = 'A wallet for storaging your cards'},
